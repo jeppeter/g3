b011cc504c95282817bb42d9a3c57555cc62688d
jeppeter <jeppeter@gmail.com>
2013-10-29 10:32:38 +0800

    to add CDirectInput8AHook

--- ioinject/detourdinput.cpp
+++ ioinject/detourdinput.cpp
@@ -12,6 +12,38 @@
 
 
 
+static std::vector<IDirectInput8A*> st_DI8AVecs;
+static CRITICAL_SECTION st_DI8ACS;
+
+ULONG UnRegisterDirectInput8AHook(IDirectInput8A* ptr)
+{
+    int findidx = -1;
+    ULONG uret=1;
+    unsigned int i;
+
+    EnterCriticalSection(&st_DI8ACS);
+    for(i=0; i<st_DI8AVecs.size() ; i++)
+    {
+        if(st_DI8AVecs[i] == ptr)
+        {
+            findidx = i;
+            break;
+        }
+    }
+
+    if(findidx >= 0)
+    {
+        st_DI8AVecs.erase(st_DI8AVecs.begin()+findidx);
+    }
+    LeaveCriticalSection(&st_DI8ACS);
+
+    if(findidx >= 0)
+    {
+        uret = ptr->Release();
+    }
+    return uret;
+}
+
 #define  DIRECT_INPUT_8A_IN()
 #define  DIRECT_INPUT_8A_OUT()
 class CDirectInput8AHook :public IDirectInput8A
@@ -29,7 +61,105 @@ public:
         DIRECT_INPUT_8A_OUT();
         return hr;
     }
+    COM_METHOD(ULONG,AddRef)(THIS)
+    {
+        ULONG uret;
+        DIRECT_INPUT_8A_IN();
+        uret = m_ptr->AddRef();
+        DIRECT_INPUT_8A_OUT();
+        return uret;
+    }
+    COM_METHOD(ULONG,Release)(THIS)
+    {
+        ULONG uret;
+        DIRECT_INPUT_8A_IN();
+        uret = m_ptr->Release();
+        DIRECT_INPUT_8A_OUT();
+        if(uret == 1)
+        {
+            uret = UnRegisterDirectInput8AHook(this->m_ptr);
+            if(uret == 0)
+            {
+                delete this;
+            }
+        }
+        return uret;
+    }
 
+    COM_METHOD(HRESULT,CreateDevice)(THIS_ REFGUID rguid,
+                                     LPDIRECTINPUTDEVICE * lplpDirectInputDevice,
+                                     LPUNKNOWN pUnkOuter)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->CreateDevice(rguid,lplpDirectInputDevice,pUnkOuter);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+    COM_METHOD(HRESULT,EnumDevices)(THIS_ DWORD dwDevType,LPDIENUMDEVICESCALLBACK lpCallback,LPVOID pvRef,DWORD dwFlags)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->EnumDevices(dwDevType,lpCallback,pvRef,dwFlags);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+    COM_METHOD(HRESULT,GetDeviceStatus)(THIS_  REFGUID rguidInstance)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->GetDeviceStatus(rguidInstance);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+    COM_METHOD(HRESULT,RunControlPanel)(THIS_ HWND hwndOwner,DWORD dwFlags)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->RunControlPanel(hwndOwner,dwFlags);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+    COM_METHOD(HRESULT,Initialize)(THIS_  HINSTANCE hinst,DWORD dwVersion)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->Initialize(hinst,dwVersion);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+
+    COM_METHOD(HRESULT,FindDevice)(THIS_ REFGUID rguidClass,LPCTSTR ptszName,LPGUID pguidInstance)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->FindDevice(rguidClass,ptszName,pguidInstance);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+    COM_METHOD(HRESULT,EnumDevicesBySemantics)(THIS_ LPCTSTR ptszUserName,LPDIACTIONFORMAT lpdiActionFormat,LPDIENUMDEVICESBYSEMANTICSCB lpCallback,LPVOID pvRef,DWORD dwFlags)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->EnumDevicesBySemantics(ptszUserName,lpdiActionFormat,lpCallback,pvRef,dwFlags);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
+
+    COM_METHOD(HRESULT,ConfigureDevices)(THIS_ LPDICONFIGUREDEVICESCALLBACK lpdiCallback,LPDICONFIGUREDEVICESPARAMS lpdiCDParams,DWORD dwFlags,LPVOID pvRefData)
+    {
+        HRESULT hr;
+        DIRECT_INPUT_8A_IN();
+        hr = m_ptr->ConfigureDevices(lpdiCallback,lpdiCDParams,dwFlags,pvRefData);
+        DIRECT_INPUT_8A_OUT();
+        return hr;
+    }
 
 };
 
