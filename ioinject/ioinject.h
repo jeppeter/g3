9f7f56301a36bc6df983a19542baea8d69e6fecb
jeppeter <jeppeter@gmail.com>
2013-11-18 15:09:40 +0800

    to add IO_CAP_CONTROL_t

--- ioinject/ioinject.h
+++ ioinject/ioinject.h
@@ -19,14 +19,21 @@
 void FiniIoInject(HMODULE hModule);
 BOOL InitIoInject(HMODULE hModule);
 
+#define  IO_INJECT_STOP            0
+#define  IO_INJECT_START           1
+
 typedef struct
 {
+	int opcode;
 	unsigned char memsharename[256];
 	int memsharesize;	
-	int memsharesectsize;                       /**/
+	int memsharesectsize;                       /*section size*/
+	int memsharenum;
+	unsigned char freeevtbasename[256];
+	unsigned char inputevtbasename[256];
 } IO_CAP_CONTROL_t,*PIO_CAP_CONTROL_t;
 
-IOINJECT_API BOOL IoInjectDummyExport(void);
+IOINJECT_API BOOL IoInjectControl(PIO_CAP_CONTROL_t pControl);
 
 #endif /*__IO_INJECT_H__*/
 
