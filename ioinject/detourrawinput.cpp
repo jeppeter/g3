b87b5481826f82a3cbd26f2c5bed14d3cfa10dfb
jeppeter <jeppeter@gmail.com>
2013-11-25 10:21:03 +0800

    change debug information for rawinput

--- ioinject/detourrawinput.cpp
+++ ioinject/detourrawinput.cpp
@@ -16,25 +16,16 @@ typedef BOOL (WINAPI *RegisterRawInputDevicesFunc_t)(
     UINT cbSize
 );
 
-GetRawInputDataFunc_t GetRawInputDataNext=GetRawInputData;
-RegisterRawInputDevicesFunc_t RegisterRawInputDevicesNext=RegisterRawInputDevices;
+typedef UINT(WINAPI *GetRawInputDeviceInfoFunc_t)(
+    HANDLE hDevice,
+    UINT uiCommand,
+    LPVOID pData,
+    PUINT pcbSize
+);
 
 
 #include "detourrawinput_debug.cpp"
 
-int __RawInputDetour(void)
-{
-    DEBUG_BUFFER_FMT(GetRawInputDataNext,10,"Before GetRawInputDataNext(0x%p)",GetRawInputDataNext);
-    DEBUG_BUFFER_FMT(RegisterRawInputDevicesNext,10,"Before RegisterRawInputDeviceNext(0x%p)",RegisterRawInputDevicesNext);
-    DetourTransactionBegin();
-    DetourUpdateThread(GetCurrentThread());
-    DetourAttach((PVOID*)&RegisterRawInputDevicesNext,RegisterRawInputDevicesCallBack);
-    DetourAttach((PVOID*)&GetRawInputDataNext,GetRawInputDataCallBack);
-    DetourTransactionCommit();
-    DEBUG_BUFFER_FMT(GetRawInputDataNext,10,"After GetRawInputDataNext(0x%p)",GetRawInputDataNext);
-    DEBUG_BUFFER_FMT(RegisterRawInputDevicesNext,10,"After RegisterRawInputDeviceNext(0x%p)",RegisterRawInputDevicesNext);
-    return 0;
-}
 
 
 BOOL DetourRawInputInit(void)
--- ioinject/detourrawinput_debug.cpp
+++ ioinject/detourrawinput_debug.cpp
@@ -1,5 +1,7 @@
 
 
+GetRawInputDataFunc_t GetRawInputDataNext=GetRawInputData;
+RegisterRawInputDevicesFunc_t RegisterRawInputDevicesNext=RegisterRawInputDevices;
 
 
 BOOL WINAPI RegisterRawInputDevicesCallBack(
@@ -35,14 +37,29 @@ UINT WINAPI GetRawInputDataCallBack(
     UINT cbSizeHeader)
 {
     UINT uret;
+    RAWINPUT *pRaw=NULL;
 
     uret = GetRawInputDataNext(hRawInput,uiCommand,pData,pcbSize,cbSizeHeader);
     if(uret != (UINT)-1)
     {
         if(pData)
         {
-            DEBUG_BUFFER_FMT(pData,*pcbSize,"uiCommand rawinput(0x%08x) 0x%08x(%d) sizeheader(%d) uret(%d)",
-                             hRawInput,uiCommand,uiCommand,cbSizeHeader,uret);
+            pRaw = (RAWINPUT*)pData;
+            if(pRaw->header.dwType == RIM_TYPEKEYBOARD)
+            {
+                DEBUG_BUFFER_FMT(pData,*pcbSize,"KeyBoard rawinput(0x%08x) uiCommand 0x%08x(%d) sizeheader(%d) uret(%d)",
+                                 hRawInput,uiCommand,uiCommand,cbSizeHeader,uret);
+            }
+            else if(pRaw->header.dwType == RIM_TYPEMOUSE)
+            {
+                DEBUG_BUFFER_FMT(pData,*pcbSize,"Mouse rawinput(0x%08x) uiCommand 0x%08x(%d) sizeheader(%d) uret(%d)",
+                                 hRawInput,uiCommand,uiCommand,cbSizeHeader,uret);
+            }
+            else
+            {
+                DEBUG_BUFFER_FMT(pData,*pcbSize,"UnknownType (%d) rawinput(0x%08x) uiCommand 0x%08x(%d) sizeheader(%d) uret(%d)",
+                                 pRaw->header.dwType,hRawInput,uiCommand,uiCommand,cbSizeHeader,uret);
+            }
         }
         else
         {
@@ -61,4 +78,19 @@ int DetourRawInputControl(PIO_CAP_CONTROL_t pControl)
     SetLastError(ret);
     return -ret;
 }
+
+int __RawInputDetour(void)
+{
+    DEBUG_BUFFER_FMT(GetRawInputDataNext,10,"Before GetRawInputDataNext(0x%p)",GetRawInputDataNext);
+    DEBUG_BUFFER_FMT(RegisterRawInputDevicesNext,10,"Before RegisterRawInputDeviceNext(0x%p)",RegisterRawInputDevicesNext);
+    DetourTransactionBegin();
+    DetourUpdateThread(GetCurrentThread());
+    DetourAttach((PVOID*)&RegisterRawInputDevicesNext,RegisterRawInputDevicesCallBack);
+    DetourAttach((PVOID*)&GetRawInputDataNext,GetRawInputDataCallBack);
+    DetourTransactionCommit();
+    DEBUG_BUFFER_FMT(GetRawInputDataNext,10,"After GetRawInputDataNext(0x%p)",GetRawInputDataNext);
+    DEBUG_BUFFER_FMT(RegisterRawInputDevicesNext,10,"After RegisterRawInputDeviceNext(0x%p)",RegisterRawInputDevicesNext);
+    return 0;
+}
+
 
