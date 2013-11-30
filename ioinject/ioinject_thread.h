2ca36f26b538ed6fe4285ad65221f5e90785be3e
jeppeter <jeppeter@gmail.com>
2013-11-30 13:54:22 +0800

    to add ioinject_thread.h

---
+++ ioinject/ioinject_thread.h
@@ -0,0 +1,12 @@
+
+/**************************************
+this file is the file to 
+**************************************/
+#ifndef  __IO_INJECT_THREAD_H__
+#define __IO_INJECT_THREAD_H__
+
+
+
+#endif  /*__IO_INJECT_THREAD_H__*/
+
+
