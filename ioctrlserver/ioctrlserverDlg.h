5374a9e12a91a51c034be796dbf2e8528edb85d6
jeppeter <jeppeter@gmail.com>
2013-12-03 17:23:20 +0800

    to add ioctrlserver and ioctrlclient into repo

---
+++ ioctrlserver/ioctrlserverDlg.h
@@ -0,0 +1,31 @@
+
+// ioctrlserverDlg.h : header file
+//
+
+#pragma once
+
+
+// CioctrlserverDlg dialog
+class CioctrlserverDlg : public CDialogEx
+{
+// Construction
+public:
+	CioctrlserverDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+	enum { IDD = IDD_IOCTRLSERVER_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
