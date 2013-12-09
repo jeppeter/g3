4e72883cd062cd172a86e33bcc4b7298cbdb8c7d
jeppeter <jeppeter@gmail.com>
2013-12-09 13:37:53 +0800

    to make the resource ok for ioctrlserver and ioctrlclient

--- ioctrlserver/Resource.h
+++ ioctrlserver/Resource.h
@@ -1,18 +1,25 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by ioctrlserver.rc
+// Microsoft Visual C++ 生成的包含文件。
+// 供 ioctrlserver.rc 使用
 //
-#define IDR_MAINFRAME					128
-#define IDD_IOCTRLSERVER_DIALOG				102
+#define IDD_IOCTRLSERVER_DIALOG         102
+#define IDR_MAINFRAME                   128
+#define IDC_BTN_EXE                     1000
+#define IDC_BTN_DLL                     1001
+#define IDC_EDT_EXE                     1002
+#define IDC_EDT_PARAM                   1003
+#define IDC_EDT_DLL                     1004
+#define IDC_EDT_BUFNUM                  1008
+#define IDC_EDT_BUFSIZE                 1010
+#define IDC_EDT_WAITTIME                1012
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
