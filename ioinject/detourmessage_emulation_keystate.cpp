c490fb55db692efd7151fd4b5552563743bb43bf
jeppeter <jeppeter@gmail.com>
2013-12-12 18:50:56 +0800

    to make VK_MENU into keystate.cpp

--- ioinject/detourmessage_emulation_keystate.cpp
+++ ioinject/detourmessage_emulation_keystate.cpp
@@ -1,8 +1,9 @@
 
 static unsigned int st_VirtKeyState[256]={0};
 static CRITICAL_SECTION st_EmuKeyStateCS;
-static int IsAltPressed()
+static int IsMenuPressed()
 {
+	return st_VirtKeyState[VK£ßMENU];
 }
 
 
