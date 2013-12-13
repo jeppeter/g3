5a08de9d8b7e23a74f5c09d496ebbd170ff5a535
jeppeter <jeppeter@gmail.com>
2013-12-13 11:07:37 +0800

    to add for 105 as capschar

--- ioinject/detourmessage_emulation_keystate.cpp
+++ ioinject/detourmessage_emulation_keystate.cpp
@@ -10,10 +10,90 @@ static CRITICAL_SECTION st_EmuKeyStateCS;
 #define  MAP_CHAR_BACKSPACE      0x08
 #define  MAP_CHAR_TAB            0x09
 
-static uint8_t st_CapsChar[256] = {
-		MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL          ,MAP_CHAR_NULL,            /*5*/
-		MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,0x08
-	};
+#define  MAP_CHAR_ENTER          0x0d
+
+#define  MAP_CHAR_ESCAPE         0x1b
+
+#define  MAP_CHAR_SPACE          0x20
+
+#define  MAP_CHAR_NUMPAD_STAR    0x2a
+#define  MAP_CHAR_NUMPAD_PLUS    0x2b
+#define  MAP_CHAR_NUMPAD_MINUS   0x2d
+#define  MAP_CHAR_MINUS          0x2d
+#define  MAP_CHAR_NUMPAD_SLASH   0x2f
+
+
+
+#define  MAP_CHAR_0              0x30
+#define  MAP_CHAR_1              0x31
+#define  MAP_CHAR_2              0x32
+#define  MAP_CHAR_3              0x33
+#define  MAP_CHAR_4              0x34
+#define  MAP_CHAR_5              0x35
+#define  MAP_CHAR_6              0x36
+#define  MAP_CHAR_7              0x37
+#define  MAP_CHAR_8              0x38
+#define  MAP_CHAR_9              0x39
+
+#define  MAP_CHAR_COLON          0x3b
+#define  MAP_CHAR_EQUAL          0x3d
+
+#define  MAP_CHAR_A              0x41
+#define  MAP_CHAR_B              0x42
+#define  MAP_CHAR_C              0x43
+#define  MAP_CHAR_D              0x44
+#define  MAP_CHAR_E              0x45
+#define  MAP_CHAR_F              0x46
+#define  MAP_CHAR_G              0x47
+#define  MAP_CHAR_H              0x48
+#define  MAP_CHAR_I              0x49
+#define  MAP_CHAR_J              0x4a
+#define  MAP_CHAR_K              0x4b
+#define  MAP_CHAR_L              0x4c
+#define  MAP_CHAR_M              0x4d
+#define  MAP_CHAR_N              0x4e
+#define  MAP_CHAR_O              0x4f
+#define  MAP_CHAR_P              0x50
+#define  MAP_CHAR_Q              0x51
+#define  MAP_CHAR_R              0x52
+#define  MAP_CHAR_S              0x53
+#define  MAP_CHAR_T              0x54
+#define  MAP_CHAR_U              0x55
+#define  MAP_CHAR_V              0x56
+#define  MAP_CHAR_W              0x57
+#define  MAP_CHAR_X              0x58
+#define  MAP_CHAR_Y              0x59
+#define  MAP_CHAR_Z              0x5a
+
+#define  MAP_CHAR_LBRACKET       0x5b
+#define  MAP_CHAR_BACKSLASH      0x5c
+#define  MAP_CHAR_RBRACKET       0x5d
+#define  MAP_CHAR_APOSTROPHY     0x60
+
+static uint8_t st_CapsChar[256] =
+{
+    MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL          ,MAP_CHAR_NULL          ,            /*5*/
+    MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_BACKSPACE     ,MAP_CHAR_TAB           ,            /*10*/
+    MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_ENTER         ,MAP_CHAR_NULL          ,            /*15*/
+    MAP_CHAR_NULL		,MAP_CHAR_NULL		 ,MAP_CHAR_NULL 	  ,MAP_CHAR_NULL		  ,MAP_CHAR_NULL		  , 		   /*20*/
+    MAP_CHAR_NULL		,MAP_CHAR_NULL		 ,MAP_CHAR_NULL 	  ,MAP_CHAR_NULL		  ,MAP_CHAR_NULL		  , 		   /*25*/
+    MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_ESCAPE     ,MAP_CHAR_NULL          ,MAP_CHAR_NULL          ,            /*30*/
+    MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_SPACE      ,MAP_CHAR_NULL          ,MAP_CHAR_NULL          ,            /*35*/
+	MAP_CHAR_NULL		,MAP_CHAR_NULL		 ,MAP_CHAR_NULL	      ,MAP_CHAR_NULL		  ,MAP_CHAR_NULL		  , 		   /*40*/
+	MAP_CHAR_NULL		,MAP_CHAR_NULL		 ,MAP_CHAR_NULL 	  ,MAP_CHAR_NULL		  ,MAP_CHAR_NULL		  , 		   /*45*/
+	MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_0             ,MAP_CHAR_1             ,            /*50*/
+	MAP_CHAR_2          ,MAP_CHAR_3          ,MAP_CHAR_4          ,MAP_CHAR_5             ,MAP_CHAR_6             ,            /*55*/
+	MAP_CHAR_7          ,MAP_CHAR_8          ,MAP_CHAR_9          ,MAP_CHAR_NULL          ,MAP_CHAR_NULL          ,            /*60*/
+	MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL          ,MAP_CHAR_NULL          ,            /*65*/
+	MAP_CHAR_A          ,MAP_CHAR_B          ,MAP_CHAR_C          ,MAP_CHAR_D             ,MAP_CHAR_E             ,            /*70*/
+	MAP_CHAR_F          ,MAP_CHAR_G          ,MAP_CHAR_H          ,MAP_CHAR_I             ,MAP_CHAR_J             ,            /*75*/
+	MAP_CHAR_K          ,MAP_CHAR_L          ,MAP_CHAR_M          ,MAP_CHAR_N             ,MAP_CHAR_O             ,            /*80*/
+	MAP_CHAR_P          ,MAP_CHAR_Q          ,MAP_CHAR_R          ,MAP_CHAR_S             ,MAP_CHAR_T             ,            /*85*/
+	MAP_CHAR_U          ,MAP_CHAR_V          ,MAP_CHAR_W          ,MAP_CHAR_X             ,MAP_CHAR_Y             ,            /*90*/
+	MAP_CHAR_Z          ,MAP_CHAR_NULL       ,MAP_CHAR_NULL       ,MAP_CHAR_NULL          ,MAP_CHAR_NULL          ,            /*95*/
+	MAP_CHAR_NULL		,MAP_CHAR_NULL		 ,MAP_CHAR_NULL 	  ,MAP_CHAR_NULL		  ,MAP_CHAR_NULL		  , 		   /*100*/
+	MAP_CHAR_NULL		,MAP_CHAR_NULL		 ,MAP_CHAR_NULL 	  ,MAP_CHAR_NULL		  ,MAP_CHAR_NULL		  , 		   /*105*/
+};
 
 static int __IsMenuPressedNoLock()
 {
