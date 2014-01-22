400041215c5d0165cbfc6b2b5b4230ff458408ad
jeppeter <jeppeter@gmail.com>
2014-01-22 16:25:56 +0800

    to add gui in ioctrlserver

--- ioctrlserver/Resource.h
+++ ioctrlserver/Resource.h
@@ -11,13 +11,20 @@
 #define IDC_EDT_DLL                     1004
 #define ID_START                        1005
 #define IDC_BTN_START                   1005
+#define IDC_BTN_GETCURSOR               1005
 #define IDC_BTN_ATTACH                  1006
 #define IDC_EDT_PID                     1007
 #define IDC_EDT_ATTACHPID               1007
 #define IDC_EDT_BUFNUM                  1008
+#define IDC_BTN_START2                  1009
 #define IDC_EDT_BUFSIZE                 1010
 #define IDC_EDT_PORT                    1011
 #define IDC_EDT_WAITTIME                1012
+#define IDC_CHK_HIDECURSOR              1013
+#define IDC_CHK_SETCURSORPOS            1014
+#define IDC_BTN_SAVECURSORBMP           1015
+#define IDC_EDT_DLL2                    1016
+#define IDC_EDT_SAVECURSORBMP           1016
 
 // Next default values for new objects
 // 
@@ -25,7 +32,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
