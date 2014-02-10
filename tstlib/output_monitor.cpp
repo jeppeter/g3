32f40dec3661cd26a7dd56a269280712a3fb1512
jeppeter <jeppeter@gmail.com>
2014-02-10 16:05:52 +0800

    to make outmon ok

--- tstlib/output_monitor.cpp
+++ tstlib/output_monitor.cpp
@@ -438,6 +438,7 @@ int OutputMonitor::__IsInProcessPids()
 int OutputMonitor::__InsertDbWinBuffer(PDBWIN_BUFFER_t pBuffer)
 {
     int ret = -ERROR_BAD_ENVIRONMENT;
+	INFOOUT("insert[%d]%s\n",pBuffer->dwProcessId,pBuffer->data);
     EnterCriticalSection(&(this->m_CS));
     if(this->m_Started && this->m_pAvailBuffers)
     {
@@ -504,9 +505,13 @@ DWORD OutputMonitor::__ProcessImpl()
     hWaitHandle[0] = this->m_hDBWinDataReady;
     hWaitHandle[1] = this->m_ThreadControl.exitevt;
 
+	/*make sure buffer ready for client handle*/
+	SetEvent(this->m_hDBWinBufferReady);
+
     while(this->m_ThreadControl.running)
     {
         dret = WaitForMultipleObjectsEx(2,hWaitHandle,FALSE,INFINITE,TRUE);
+		INFOOUT("dret %d\n",dret);
         if(dret == WAIT_OBJECT_0)
         {
             ret = this->__HandleBufferIn();
