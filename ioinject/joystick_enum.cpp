ed7232de2a72b939b4ad285cf5ce6efafb46f20d
jeppeter <jeppeter@gmail.com>
2014-04-05 22:03:46 +0800

    to start coding GetConfig

--- ioinject/joystick_enum.cpp
+++ ioinject/joystick_enum.cpp
@@ -14,12 +14,26 @@ JoyStickBetopEnum::~JoyStickBetopEnum()
 
 HRESULT JoyStickBetopEnum::GetConfig(UINT uiJoy,LPDIJOYCONFIG pjc,DWORD dwFlags)
 {
+    HRESULT hr=DIERR_NOMOREITEMS;
     if(uiJoy != 0)
     {
         return DIERR_NOMOREITEMS;
     }
 
-	
+    hr = DIERR_INVALIDPARAM;
+    switch(dwFlags)
+    {
+    case DIJC_GUIDINSTANCE:
+        break;
+    case DIJC_REGHWCONFIGTYPE:
+        break;
+    case DIJC_GAIN:
+        break;
+    case DIJC_CALLOUT:
+        break;
+    }
+
+    return hr;
 }
 
 /* now we should */
